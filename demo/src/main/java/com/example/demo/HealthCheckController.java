c168285e39652c8c942591f02016a82e750d95fd
nghiabt <nghiabt@nal.vn>
2025-04-16 10:28:01 +0700

    ADD: project test

--- demo/src/main/java/com/example/demo/HealthCheckController.java
+++ demo/src/main/java/com/example/demo/HealthCheckController.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
 public class HealthCheckController {
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
 }
